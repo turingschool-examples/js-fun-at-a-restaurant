74582650122db18a880423addee0181f58dc6150
Nathaniel Millard <nathanielmillard@gmail.com>
2020-07-04 14:44:18 -0600

    Missed commit from finishing earlier

--- src/warm-up.js
+++ src/warm-up.js
@@ -9,30 +9,33 @@
 // Initialize three new variables, "food", "type" and "inStock", and assign values to them with
 // a food's name, type and number of items in stock.
 
-// var food = "Ice Cream";
-// var type = "Dessert";
-// var inStock = 10
+var food = "Ice Cream";
+var type = "Dessert";
+var inStock = 10
 
 
 
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
 
+inStock = (inStock * 2)
 
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
 
+inStock = (inStock - 1)
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
 
-
+var isFavorite = false
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+var shouldOrderMore = (inStock < 20)
 
 
 //------------------------- Conditionals -------------------------//
@@ -40,17 +43,35 @@
 
 
 // Express the following in code: If the variable "stockCount" is less than 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
 
+function countStock(stockCount) {
+  if (stockCount < 1) {
+    return `I'm going to east some ${food}!`
+  }
+  else {
+    return `We need to get some more ${food}`
+  };
+}
 
+console.log(countStock(3))
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
 
+function checkFavorite() {
+  if (isFavorite == true) {
+    return `My favorite food is ${food}.`
+  }
+  else {
+    return `I recently ate ${food}.`
+  }
+}
 
+console.log(checkFavorite())
 
 //------------------------- Arrays -------------------------//
 
@@ -58,17 +79,35 @@
 // Create a new variable "vegetables" and assign it to an Array of 5 different
 // vegetables, represented by their names as Strings.
 
+var vegetables = [
+  "broccoli",
+  "peas",
+  "carrots",
+  "cauliflower",
+  "turnips",
+]
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
 
+console.log(vegetables[2])
 
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
 
+var fruits = [
+  "apple",
+  "orange",
+  "kiwi",
+  "strawberry",
+  "melon",
+  "mango",
+  "blueberry"
+]
 
 // Write the code to access the 5th element of the array, using bracket notation.
 
+console.log(fruits[4])
 
 //------------------------- Objects -------------------------//
 
@@ -76,7 +115,14 @@
 // keys: name, and isOpen. The value for name should the string 'King Soopers', and
 // the value for isOpen should be true
 
-
+var groceryStore = {
+  name: 'King Soopers',
+  isOpen: true,
+}
 
 // Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
 // notation to do this. DO NOT modify your code from above.
+
+groceryStore.location = 'Denver, Colorado'
+
+console.log(groceryStore)
