415fadb9577795820b8803acb68f17e7439e4d09
Estelle Staffieri <estellelareinne@gmail.com>
2020-07-05 14:27:40 -0600

    create addMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,26 @@
+function createRestaurant(name) {
+  return {
+      name: name,
+      menus: {
+        breakfast: [],
+        lunch: [],
+        dinner: [],
+      }
+    };
+}
 
+function addMenuItem(restObj, item) {
+  var mealType = item.type;
+  var menusArray = restObj.menus[mealType];
+    if (menusArray.includes(item)) {
+      return restObj.menus
+    } else {
+    menusArray.push(item);
+    }
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
+  createRestaurant,
+  addMenuItem,
   // removeMenuItem
 }
