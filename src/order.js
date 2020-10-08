73bbf4a0be196f43aeb3bf9618d2ee68a8e30b9d
Ben Prat <bvprat@gmail.com>
2020-10-08 00:17:30 -0600

    Complete takeOrder assertions.

--- src/order.js
+++ src/order.js
@@ -1,6 +1,17 @@
+function takeOrder(order, orderList) {
+  if (orderList.length < 3) {
+    orderList.push(order)
+  }
+}
+
+
+
+
+
+
 
 module.exports = {
-  // takeOrder,
+  takeOrder,
   // refundOrder,
   // listItems,
   // searchOrder
