7b0e4dbe57e272f9c79471b2ac95c3e893ec76be
Ben Prat <bvprat@gmail.com>
2020-10-11 11:56:16 -0600

    Complete Chef Iteration

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,31 @@
 class Chef {
-  constructor() {
-    
+  constructor(name,restaurant) {
+  this.name = name;
+  this.restaurant = restaurant;
+
+  }
+
+  greetCustomer(name, morning) {
+    if (morning === true) {
+      return `Good morning, ${name}!`
+    } else {
+      return `Hello, ${name}!`
+    }
+ }
+
+  checkForFood(foodItem) {
+    if (this.restaurant.menus[foodItem.type].includes(foodItem))  {
+    return `Yes, we're serving ${foodItem.name} today!`
+  } else {
+    return `Sorry, we aren't serving ${foodItem.name} today.`
+    }
   }
 }
+
+
+
+
+
+
 
 module.exports = Chef;
