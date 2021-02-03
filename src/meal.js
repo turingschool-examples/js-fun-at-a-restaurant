8b6f8d1909a771c1117f7546d3af6503914276cf
Steve <callasteven@gmail.com>
2021-02-03 14:18:56 -0700

    Pass all mealjs tests q

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,50 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  var menuItemName = `Delicious ${food}`;
+  return menuItemName;
+}
 
+function createMenuItem(item, price, type) {
+  var menuItem = {
+    name: item,
+    price: price,
+    type: type,
+  }
+  return menuItem;
+}
+
+function addIngredients(item, ingredients) {
+  if (!ingredients.includes(item)) {
+    ingredients.push(item);
+    return ingredients;
+  }
+//
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(price) {
+  return price * .9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+  return recipe;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
--- src/warm-up.js
+++ src/warm-up.js
@@ -4,79 +4,70 @@
 // iteration 1 until you are confident that you've completed all of iteration 0.
 
 //------------------------- Variables -------------------------//
-
-
 // Initialize three new variables, "food", "type" and "inStock", and assign values to them with
 // a food's name, type and number of items in stock.
-
-// var food = "Ice Cream";
-// var type = "Dessert";
-// var inStock = 10
-
-
-
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
-// the inStock count double what it currently is.
-
-
+var food = "Ice Cream";
+var type = "Dessert";
+var inStock = 10
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! 
+// Use a built in math operator to make the inStock count double what it currently is.
+inStock = inStock * 2;
+console.log(inStock);
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
 // Use a built in math operator to decrement the inStock count by 1.
-
-
+inStock = inStock - 1;
+console.log(inStock);
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
-
-
-
-// Create a new variable named "shouldOrderMore", and use a comparison operator to
-// assign a boolean value to this variable. If "inStock" is less than 20, the
-// variable should be true, otherwise it should be false. You MUST use a
-// comparison operator.
-
-
-
+var isFavorite = true;
+console.log(isFavorite);
+// Create a new variable named "shouldOrderMore", and use a comparison operator to assign a boolean value to this variable. If "inStock" is less than 20, the variable should be true, otherwise it should be false. You MUST use a comparison operator.
+var shouldOrderMore = inStock < 20;
+console.log(shouldOrderMore);
 //------------------------- Conditionals -------------------------//
+// Express the following in code: If the variable "stockCount" is greater or equal to 1, log the statement 'I'm going to eat some [food]!', otherwise, log the statement // 'We need to get some more [food].'
 
-
-
-// Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
-// 'We need to get some more [food].'
-
-
-
+//changed to "inStock" to match the variable on line 5
+function evaluateStockCount() {
+  if (inStock >= 1) {
+    return `I\'m going to eat some ${food}!`;
+  } else {
+    return `We need to get some more ${food}.`;
+  }
+}
+console.log(evaluateStockCount());
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite food is [food]!'. Otherwise, log the
 // statement 'I recently ate [food]'. In either case, the value stored in
 // the "food" variable should be included in the logged statement.
-
-
-
+function myFavoriteFood() {
+  if (isFavorite === true) {
+    return `My favorite food is ${food}!`;
+  } else {
+    return `I recently ate ${food}.`;
+  }
+}
+console.log(myFavoriteFood());
 //------------------------- Arrays -------------------------//
-
-
-// Create a new variable "vegetables" and assign it to an Array of 5 different
-// vegetables, represented by their names as Strings.
-
-
+// Create a new variable "vegetables" and assign it to an Array of 5 different // vegetables, represented by their names as Strings.
+var vegetables = ['carrots', 'cucumber', 'lettuce', 'tomato', 'avocado'];
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
-
+console.log(vegetables[2]);
 // Create a new variable "fruits" and assign it to an Array of 7
 // different fruits.
-
-
+var fruits = ['apple', 'raspberry', 'blueberry', 'banana', 'orange', 'pear', 'plum'];
 // Write the code to access the 5th element of the array, using bracket notation.
-
-
+console.log(vegetables[4]);
 //------------------------- Objects -------------------------//
-
-// Create a new variable called "groceryStore" and assign to it an object literal with two
-// keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
+// Create a new variable called "groceryStore" and assign to it an object literal with two // keys: name, and isOpen. The value for name should the string 'King Soopers' (or your favorite grocery store), and
 // the value for isOpen should be true. Which other key value pairs would make sense to add to this object?
+var groceryStore = {
+  name: 'King Soopers', 
+  isOpen: true,
+}
+// Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot notation to do this. DO NOT modify your code from above.
+groceryStore.location = 'Denver, Colorado';
+console.log(groceryStore);
 
-
-
-// Write the code to add the key of location, with the value of 'Denver, Colorado'. Use dot
-// notation to do this. DO NOT modify your code from above.
+//repl.it for this assigment https://repl.it/@SteveCalla/WeepyFickleLamp#index.js
