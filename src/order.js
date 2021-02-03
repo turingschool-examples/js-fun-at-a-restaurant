b7f433eb29d640fe20aa3323f69e9a11c1fe4442
Steve <callasteven@gmail.com>
2021-02-03 15:26:37 -0700

    Pass all orderjs tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,35 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  }
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  deliveryOrders.splice(orderNum - 1, 1);
+}
+
+function listItems(deliveryOrders) {
+  var items = '';
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items += `${deliveryOrders[i].item}, `;
+  }
+  var finalItems = items.slice(0, -2);
+  return finalItems;
+}
+
+function searchOrder(deliveryOrders, orderItem) {
+  var containsOrder = false;
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item.includes(orderItem)) {
+      containsOrder = true;
+    }
+  }
+  return containsOrder;
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
