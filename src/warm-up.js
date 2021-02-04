5596e74dea4bd7d3bb3434653c5f4a8d84f23f34
Nichele Dunn <the7yearitch@hotmail.com>
2021-02-03 17:21:48 -0700

    Begin work on Iteration0

--- src/warm-up.js
+++ src/warm-up.js
@@ -5,23 +5,22 @@
 
 //------------------------- Variables -------------------------//
 
-
 // Initialize three new variables, "food", "type" and "inStock", and assign values to them with
 // a food's name, type and number of items in stock.
+var food = "Ice Cream";
+var type = "Dessert";
+var inStock = 10;
 
-// var food = "Ice Cream";
-// var type = "Dessert";
-// var inStock = 10
-
-
-
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
+var reStock = inStock * 2;
+console.log(reStock);
 
-
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
-
+var oopsNeedASnack = reStock--;
+console.log(oopsNeedASnack);
+// this seems to work correctly in the console, but for some reason isn't updating in runJS
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
@@ -40,7 +39,7 @@
 
 
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
 
 
