d6a31a39027b860b1a6086419fb9d9622f1e92b4
Lindsay Schnell <lschnell8@gmail.com>
2021-05-13 21:43:54 -0600

    Complete Chef.js file

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,20 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
-}
+  greetCustomer(name, morning) {
+    if(morning) {
+      return `Good morning, ${name}!`
+    }
+      return `Hello, ${name}!`
+  }
+  checkForFood(foodItem) {
+    if(this.restaurant.menus.breakfast.includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    }
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+  }
+};
 
 module.exports = Chef;
--- test/Chef-test.js
+++ test/Chef-test.js
@@ -6,27 +6,26 @@ var {
   addMenuItem
 } = require("../src/restaurant");
 
-
 describe("Chef (class)", function() {
-  it.skip('should instantiate a chef object', function () {
+  it('should instantiate a chef object', function () {
     var hannah = new Chef("Hannah");
 
     assert.instanceOf(hannah, Chef);
   });
 
-  it.skip("should have a name", function() {
+  it("should have a name", function() {
     var chef = new Chef("Hannah");
 
     assert.equal(chef.name, "Hannah");
   });
 
-  it.skip("should be able to have a different name", function () {
+  it("should be able to have a different name", function () {
     var chef = new Chef("Casey");
 
     assert.equal(chef.name, "Casey");
   });
 
-  it.skip("should work at a restaurant", function() {
+  it("should work at a restaurant", function() {
     var restaurant = createRestaurant("Best Bakery Ever");
     var chef = new Chef("Hannah", restaurant);
 
@@ -34,21 +33,21 @@ describe("Chef (class)", function() {
     assert.deepEqual(chef.restaurant, restaurant);
   });
 
-  it.skip("should be able to work at a different restaurant", function () {
+  it("should be able to work at a different restaurant", function () {
     var restaurant = createRestaurant("Amazing Bakery");
     var chef = new Chef("Scott", restaurant);
 
     assert.deepEqual(chef.restaurant, restaurant);
   });
 
-  it.skip("should be able to greet a customer by name", function() {
+  it("should be able to greet a customer by name", function() {
     var scott = new Chef("Scott");
 
     assert.equal(scott.greetCustomer("Will"), "Hello, Will!");
     assert.equal(scott.greetCustomer("Pam"), "Hello, Pam!");
   });
 
-  it.skip('should greet a customer differently if it is morning', function () {
+  it('should greet a customer differently if it is morning', function () {
     var casey = new Chef("Casey");
 
     assert.equal(casey.greetCustomer("Scott"), "Hello, Scott!");
@@ -56,14 +55,14 @@ describe("Chef (class)", function() {
     assert.equal(casey.greetCustomer("Hannah", false), "Hello, Hannah!");
   });
 
-  it.skip("should confirm if a requested item is on the menu", function() {
+  it("should confirm if a requested item is on the menu", function() {
     var restaurant = createRestaurant("Average Bakery");
     var chef = new Chef("Hannah", restaurant);
 
-    var foodItem1 = { 
+    var foodItem1 = {
       name: "Cinnamon Rolls",
       price: "4.49",
-      type: "breakfast" 
+      type: "breakfast"
     };
 
     var foodItem2 = {
@@ -82,7 +81,7 @@ describe("Chef (class)", function() {
     assert.equal(foodConfirmation2, "Yes, we're serving Cherry Danishes today!");
   });
 
-  it.skip("should confirm if a different requested item is on the menu", function () {
+  it("should confirm if a different requested item is on the menu", function () {
     var restaurant = createRestaurant("Five Star Bakery");
     var chef = new Chef("Kayla", restaurant);
 
@@ -99,7 +98,7 @@ describe("Chef (class)", function() {
     assert.equal(foodConfirmation, "Yes, we're serving Tart Tatin today!");
   });
 
-  it.skip("should confirm if a requested item is not on the menu", function () {
+  it("should confirm if a requested item is not on the menu", function () {
     var restaurant = createRestaurant("Best Bakery Ever");
     var chef = new Chef("Scott", restaurant);
 
@@ -114,7 +113,7 @@ describe("Chef (class)", function() {
     assert.equal(foodConfirmation, "Sorry, we aren't serving Quiche today.");
   });
 
-  it.skip("should confirm if a different requested item is not on the menu", function () {
+  it("should confirm if a different requested item is not on the menu", function () {
     var restaurant = createRestaurant("Best Bakery Ever");
     var chef = new Chef("Scott", restaurant);
 
