238a077cdff364434d3ed2cf681627647763fe24
Kevin Cuadros <kev040798@gmail.com>
2021-05-16 16:29:29 -0400

    Finish iteration 1 Meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,52 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`
+}
 
+function createMenuItem(name, price, type){
+  var obj = {
+    name: name,
+    price: price,
+    type: type
+  }
+
+  return obj;
+}
+
+
+function addIngredients(ingredient, ingredients) {
+  if (!ingredients.includes(ingredient)){
+    ingredients.push(ingredient);
+  }
+}
+
+function formatPrice(price) {
+  return `$${price}`
+}
+
+function decreasePrice(price){
+  var discount = price - (price * 0.10)
+
+  return discount;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var obj = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+
+  return obj;
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
