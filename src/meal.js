6e0cafbd8054286bddf025f10d3c9549325cf0ad
Gabby Recny <gjtrecny@gmail.com>
2021-08-26 21:20:47 -0500

    Add meals.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return(`Delicious ${menuItemName}`);
 
 }
+
+function createMenuItem() {
+  var createMenuItem = {
+  name: 'Delicious French Toast',
+  price: 10.99,
+  type: 'breakfast'
+};
+  return createMenuItem;
+}
+
+function addIngredients(foodItem, ingredients) {
+  for (var i = 0; i < ingredients.length; i++) {
+    if (foodItem === ingredients[i]) {
+      return;
+    }
+}
+ingredients.push(foodItem);
+}
+
+
+
+
+
+
+
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  createMenuItem,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
