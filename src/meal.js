d15551aefaa534aa1fb7f0aac81a2264c4793744
Josephine Heidepriem <jheidepriem@gmail.com>
2022-10-12 17:20:40 -0500

    start creating meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,26 @@
-function nameMenuItem() {
+function nameMenuItem(foodItem) {
+  return `Delicious ${foodItem}`
+}
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem
+}
+
+function addIngredients(toppings, ingredients) {
+  if (!ingredients.includes(toppings))
+  ingredients.push(toppings);
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
