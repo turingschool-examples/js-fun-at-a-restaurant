5aaf1f9a1478a84b72e81c340fbb337acd23af00
Josephine Heidepriem <jheidepriem@gmail.com>
2022-10-12 18:32:44 -0500

    finish meal test

--- src/meal.js
+++ src/meal.js
@@ -1,6 +1,6 @@
 function nameMenuItem(foodItem) {
   return `Delicious ${foodItem}`
-}
+};
 
 function createMenuItem(name, price, type) {
   var menuItem = {
@@ -9,21 +9,39 @@ function createMenuItem(name, price, type) {
     type: type
   }
   return menuItem
-}
+};
 
 function addIngredients(toppings, ingredients) {
   if (!ingredients.includes(toppings))
   ingredients.push(toppings);
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
 }
 
+function decreasePrice(price) {
+  var discountPrice = price * .10
+  var finalPrice = price - discountPrice
+  return finalPrice
+};
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+  return recipe
+};
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
